ba632f8b9529aa39493141757063fa43afe5ef4b
JubaStriker <jubastriker@gmail.com>
2022-11-17 11:27:59 +0600

    appointmant card added

---
+++ src/Pages/Appointment/AvailableAppointment/AppointmentOption.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const AppointmentOption = ({ appointment }) => {
+
+    const { name, slots } = appointment;
+
+    return (
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body text-center ">
+                    <h2 className="card-title font-bold text-primary justify-center"> {name}</h2>
+                    {slots.length > 0 ? <p>{slots[0]}</p> : 'Try another day'}
+                    <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} available.</p>
+                    <div className="card-actions justify-center">
+                        <button className="btn text-white bg-gradient-to-r from-primary to-secondary border-0
+                        hover:text-gray-200 ">Book Appointment</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AppointmentOption;
--- src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment/AvailableAppointment.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
+import AppointmentOption from './AppointmentOption';
 
 const AvailableAppointment = ({ selectedDate }) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setAppointmentOptions(data))
+    }, [])
+
     return (
         <div>
             <h1 className='text-center text-primary font-semibold text-lg'>
                 Available Appointments on {format(selectedDate, 'PP')}
             </h1>
+
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-auto max-w-7xl my-16'>
+                {appointmentOptions.map(appointment => <AppointmentOption key={appointment._id}
+                    appointment={appointment}></AppointmentOption>)}
+            </div>
         </div>
     );
 };
